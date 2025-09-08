49079c8722f5feb65ab29e75d63c2da5dd619866
brian kramer <kramsman@yahoo.com>
2025-09-08 19:21:50 -0400

    list blank names then remove, use date of all_user file for ouput names, cleanup code

--- Match Haar lead writers.py
+++ Match Haar lead writers.py
@@ -7,12 +7,12 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import pymsgbox
-from bekutils import setup_loguru, autosize_xls_cols, bad_path_create, exit_yes_no, exit_yes, \
-    text_box, get_file_name, get_dir_name, check_ws_headers
+# from bekutils import setup_loguru, autosize_xls_cols, bad_path_create, exit_yes_no, exit_yes, \
+#     text_box, get_file_name, get_dir_name, check_ws_headers
 
 SINCERE_REQUESTS = "/Users/Denise/Library/CloudStorage/Dropbox/Postcard Files/Other/WriterLists/Haar leads " \
-            "7-2025/all-parent-campaigns-requests-2025-08-19.csv"  # requests from 1/1/2020 to 8/19/2025
-
+                   "7-2025/all-parent-campaigns-requests-2025-08-19.csv"  # requests from 1/1/2020 to 8/19/2025
+SINCERE_ALL_USERS = "/Users/Denise/Downloads/all-users-2025-09-08.csv"
 
 INPUT_DATA = [
     ["Bob",
@@ -32,12 +32,12 @@ INPUT_DATA = [
      "Name", "Email Address",
      ],
     ["Larry",
-    "/Users/Denise/Library/CloudStorage/Dropbox/Postcard Files/Other/WriterLists/Haar leads 7-2025/Haar Larry writers for ROV ver 1.xlsx",
-    "Name", "email",
+     "/Users/Denise/Library/CloudStorage/Dropbox/Postcard Files/Other/WriterLists/Haar leads 7-2025/Haar Larry writers for ROV ver 1.xlsx",
+     "Name", "email",
      ],
     ["Marlene",
-    "/Users/Denise/Library/CloudStorage/Dropbox/Postcard Files/Other/WriterLists/Haar leads 7-2025/Haar Marlene writers ROV ListForBrianKramer 7-29-20251.xlsx",
-    "NAME", "EMAIL",
+     "/Users/Denise/Library/CloudStorage/Dropbox/Postcard Files/Other/WriterLists/Haar leads 7-2025/Haar Marlene writers ROV ListForBrianKramer 7-29-20251.xlsx",
+     "NAME", "EMAIL",
      ],
     ["Jim",
      "/Users/Denise/Library/CloudStorage/Dropbox/Postcard Files/Other/WriterLists/Haar leads 7-2025/Jim Sincere "
@@ -49,20 +49,72 @@ INPUT_DATA = [
 ## below begins programming code
 SINCERE_REQUESTS = Path(SINCERE_REQUESTS)
 
+SINCERE_ALL_USERS = Path(SINCERE_ALL_USERS)
+# get date by concatenating last three items separated by '-'
+SINCERE_ALL_USERS_DATE = '-'.join(SINCERE_ALL_USERS.stem.split('-')[-3:])
+
+# noinspection SpellCheckingInspection
+def format_as_matchname(name):
+    """ format name in most standard fashion so match can be preformed """
+
+    # TODO: use optional first name last name instead of combined
+
+    try:
+        # exception added for a name read as a float/nan
+        matched_name = name.replace(' ', '').lower()
+    except:
+        matched_name = str(name)
+        a = 1
+    return matched_name
+
 
 def read_org_data(org_xls, org_name_field, org_email_field):
+    """ read bulk input file supplied by organizer and rename given name and email fields to standard."""
+
     org_data = pd.read_excel(org_xls, dtype=str)
     org_data = org_data.rename(columns={org_name_field: 'name', org_email_field: 'new_email'})
-    org_data['match_name'] = org_data['name'].str.replace(' ', '').str.lower()
+
+    org_data['match_name'] = org_data['name'].apply(format_as_matchname)
+
+    missing_name = org_data[org_data['match_name'] == ''][['match_name', 'new_email']]
+    if not missing_name.empty:
+        # TODO write to log file and not console
+        pymsgbox.alert(text=f"Some emails were blank in:\n\n   '{Path(org_xls).name}'\n\nThey will be removed.",
+                       title='Check Console', button='OK')
+        print()
+        print(Path(org_xls).name)
+        print(missing_name)
+
+    org_data = org_data[org_data['match_name'] != '']
+
     return org_data
 
 
+# TODO: report on list of rows missing name or email; can't load into sincere
+
 def read_and_group_sincere_data(sincere_requests):
-    sincere_data = pd.read_csv(sincere_requests, na_filter=False)
-    sincere_data = sincere_data.rename(columns={'writer_name': 'name', 'writer_email': 'existing_email', 'org_name': 'room'})
-    sincere_data['match_name'] = sincere_data['name'].str.replace(' ', '').str.lower()
+    """ read sincere requests file, rename name, email and room fields and total address counts by year"""
+
+    # TODO filter out or report missing emails and names
+    sincere_data = pd.read_csv(sincere_requests, na_filter=False, dtype={'name': str, })
+    sincere_data = sincere_data.rename(
+        columns={'writer_name': 'name', 'writer_email': 'existing_email', 'org_name': 'room'})
+
+    sincere_data['match_name'] = sincere_data['name'].apply(format_as_matchname)
+
+    missing_name = sincere_data[sincere_data['match_name'] == ''][['match_name', 'existing_email']]
+    if not missing_name.empty:
+        # TODO write to log file and not console
+        pymsgbox.alert(
+            text=f"Some emails were blank in:\n\n   '{Path(sincere_requests).name}'\n\nThey will be removed.",
+            title='Check Console', button='OK')
+        print()
+        print(Path(sincere_requests).name)
+        print(missing_name)
+
+    sincere_data = sincere_data[sincere_data['match_name'] != '']
     sincere_data['year'] = pd.to_datetime(sincere_data['created_at']).dt.year
-    grouped_sincere_data = sincere_data.groupby(['match_name', 'name', 'existing_email', 'room', 'year'])\
+    grouped_sincere_data = sincere_data.groupby(['match_name', 'name', 'existing_email', 'room', 'year']) \
         .agg({'addresses_count': ['sum', ]}).reset_index()
 
     grouped_sincere_data.columns = grouped_sincere_data.columns.get_level_values(0)
@@ -70,31 +122,42 @@ def read_and_group_sincere_data(sincere_requests):
 
 
 def merge_org_and_sincere_data(org_data, grouped_sincere_data):
+    """ match the writers supplied by organizer by name to show if they are already in rooms and how many addresses
+    they've written by year"""
     merged_data = org_data.merge(grouped_sincere_data, how='left', left_on='match_name', right_on='match_name',
                                  sort=False, suffixes=('_org', '_sincere',), copy=None, indicator=False, validate=None)
 
-    merged_data = merged_data[~merged_data['room'].isna()]
+    merged_data = merged_data[~merged_data['room'].isna()]  # TODO: is this needed??
     merged_data = merged_data[merged_data['room'] != "National-Bob Haar"]
     merged_data['new_email'] = merged_data['new_email'].str.lower()
     merged_data['existing_email'] = merged_data['existing_email'].str.lower()
 
+    # we know these are an existing pwriter because name and email match.
     merged_data['email_matches'] = np.where(merged_data['new_email'] == merged_data['existing_email'], 'Yes', 'No')
-    merged_data = merged_data.reset_index()
+    merged_data = merged_data.reset_index(drop=True)
     return merged_data
 
 
+# TODO link to merging / in one not in other code: https://stackoverflow.com/questions/53645882/pandas-merging-101
+
+
 def main_program(input_data):
+    """ loop through list of info for each organizers files and supply matched writer report for each"""
+    grouped_sincere_data = read_and_group_sincere_data(SINCERE_REQUESTS)
 
     for org_name, org_xls, org_name_field, org_email_field in input_data:
-
         org_data = read_org_data(org_xls, org_name_field, org_email_field)
 
-        grouped_sincere_data = read_and_group_sincere_data(SINCERE_REQUESTS)
-
+        # merged_data will contain only writers from Org's file matched by name with past address request counts or
+        # email/room for all matches
         merged_data = merge_org_and_sincere_data(org_data, grouped_sincere_data)
 
-        merged_data.to_excel(f"{org_name} batch load writer match {datetime.today().strftime('%Y-%m-%d')}.xlsx", index=True, columns=['name_org', 'email_matches',
-                             'new_email', 'existing_email', 'room', 'year', 'addresses_count'])
+        # merged_data.to_excel(f"{org_name} batch load writer match {datetime.today().strftime('%Y-%m-%d')}.xlsx",
+
+        merged_data.to_excel(Path("rpts") / f"{org_name} batch load writer match with {SINCERE_ALL_USERS_DATE} "
+                                            f"user data.xlsx",
+                             index=True, columns=['name_org', 'email_matches',
+                                                  'new_email', 'existing_email', 'room', 'year', 'addresses_count'])
 
         a = 1
 
